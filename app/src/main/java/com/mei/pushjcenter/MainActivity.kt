54f5fec55f7d5e517ff0e3f3ac959cf6da820963
meixianbing <meixb@akulaku.com>
2021-01-15 15:49:19 +0800

    在demo中引用发布到jcenter中的库

--- app/src/main/java/com/mei/pushjcenter/MainActivity.kt
+++ app/src/main/java/com/mei/pushjcenter/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.mei.pushjcenter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mei.animation.AnimationMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, AnimationMainActivity::class.java))
+
     }
 }
